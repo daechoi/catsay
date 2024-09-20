fcc8319f67bf9caa075fded5a9619df4251a5602
Dae Choi <daechoi@outlook.com>
2024-09-20 11:04:23 -0400

    adds integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use colored::*;
 use structopt::StructOpt;
 use failure::ResultExt;
+use exitfailure::ExitFailure;
+use std::io::{self, Read};
 
 #[derive(StructOpt)]
 struct Options {
@@ -11,19 +13,27 @@ struct Options {
     dead: bool,
 
     #[structopt(short ="f", long = "file", parse(from_os_str))]
-    catfile: Option<std::path::PathBuf>,
+    cat_file: Option<std::path::PathBuf>,
+
+    #[structopt(short = "i", long = "stdin")]
+    use_stdin: bool,
 }
 
-fn main() -> Result<(), failure::Error> {
+fn main() -> Result<(), ExitFailure> {
     let options = Options::from_args();
-    let message = options.message;    
+    let mut message = String::new();
+    if options.use_stdin {
+        io::stdin().read_to_string(&mut message).with_context(|_| "could not read from stdin")?;
+    } else {
+        message = options.message;
+    };
 
     let eye = if options.dead { "x" } else { "o" };
     if message.to_lowercase() == "woof" {
         eprintln!("A cat shouldn't bark like a dog!");
     }
 
-    match &options.catfile {
+    match &options.cat_file {
         Some(path) => {
             let cat_template = std::fs::read_to_string(path).with_context(|_| format!("could not read file {:?}", path))?;
             let cat_picture = cat_template.replace("{eye}", eye);
@@ -36,7 +46,6 @@ fn main() -> Result<(), failure::Error> {
             Ok(())
         }
     }
-
 }
 
 fn print_cat(message: &str, eye: &str) {
